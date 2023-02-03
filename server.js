a5503129d42afa3238c9b4df601ac5b8adf1bd43
Joshua Baldwin <baldwinjd2@appstate.edu>
2023-02-03 00:02:12 -0500

    Adds cat GIF for CD test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Welcome to Gameboxd');
+    res.send('<img src="https://i0.wp.com/www.printmag.com/wp-content/uploads/2021/02/4cbe8d_f1ed2800a49649848102c68fc5a66e53mv2.gif?resize=476%2C280&ssl=1":></img>');
 });
 
 app.listen(port, () => {
