9429d09bfd51472065cf0c579b9c45c586572f61
Joshua Baldwin <baldwinjd2@appstate.edu>
2023-02-13 14:20:04 -0500

    Fixes proper homepage

--- routes/home-route.js
+++ routes/home-route.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    console.log(__dirname);
     res.sendfile(__dirname + '/views/home.html');
 });
 
