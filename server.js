1daca534167670bc2757656d1397dbc24676aafc
Joshua Baldwin <baldwinjd2@appstate.edu>
2023-03-08 12:40:34 -0500

    Minor changes

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const createTcpPool =  require('./database.js')
 const app = express();
 const port = 8080;
 
+app.set('view engine', 'ejs');
+
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.use('/', home);
