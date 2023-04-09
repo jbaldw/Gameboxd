416cc46cd8fb5d706975e740a66d071967ec6799
Joshua Baldwin <39449188+jbaldw@users.noreply.github.com>
2023-04-08 22:44:52 -0400

    Tests users in Firestore

--- public/scripts/get-game-data.js
+++ public/scripts/get-game-data.js
@@ -1,9 +1,3 @@
 function getGameData(gameId) {
-    axios.get('/gameData', {params: {gameId: gameId}}) 
-    .then((response) => {
-        console.log(response);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+    axios.post('/addUser', {params: {userName: 'Joshua'}});
 }
